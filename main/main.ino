b917d351089d2cc16cf263637a269cccdf5b5a4b
allison98 <allison98@gmail.com>
2019-01-22 21:13:39 -0800

    demo one requirements

--- main/main.ino
+++ main/main.ino
@@ -10,24 +10,28 @@ ICSP outer cennter pin - SIN (11)
 PIN 10 - BLANK (10)
 PIN 9 - XLAT (9)
 PIN 5 -- GSCLK (3)
+VIN -- VIN
 
-
-CONNECT THE 
 */
 
 #include <Stepper.h> // For the stepper motor
+#include "Tlc5940.h"
+#include "tlc_fades.h"
 
 #define STEPS 1023 // the number of steps in one revolution of your motor (28BYJ-48)
 
 Stepper stepper(STEPS, 2, 4, 3, 6);
 
 int previous_direction = 0;
-float bc_current, bc_voltage, read_current, read_current;
+float bc_current, bc_voltage, read_current, read_voltage;
 
-direcpin = 0 //
+int direcPin = 0; //
 
 void setup() {
     Serial.begin(9600); // connect to computer
+    Tlc.init(); //PWM init 
+      stepper.setSpeed(10); 
+    
 }
 
 void loop() {   
@@ -39,17 +43,22 @@ void loop() {
     // thats the value we want it to go
     // if the value is larger than 512 use 512 if the number is closer to 0 use 0
 
-
-
 // Step 3: Compare variables and change the respective actions
     // a) measure wind direction then control stepper motor
     // b) measure booster converter values and control PWM
    //370 560 oohms 800
 
-val_direction = analogRead(direcPin);
+/*
 
-step_direction = previous_direction - val_direction; 
+|------------------------------|
+|  CONTROL LOOP 1 - DIRECTION  |
+|------------------------------|
 
+*/
+
+int val_direction = analogRead(direcPin);
+
+int step_direction = val_direction - previous_direction;
 /*
 if (step_direction > 512)
     step_direciton = 512;
@@ -62,7 +71,6 @@ stepper.step(step_direction);
 previous_direction = val_direction;
 
 
-
 /*    
     stepper.setSpeed(1); // 1 rpm
     stepper.step(2038); // do 2038 steps -- corresponds to one revolution in one minute
@@ -72,9 +80,13 @@ previous_direction = val_direction;
 
     // Use for testing motor 
 
+/*
+|------------------------------|
+|  CONTROL LOOP 2 - PWM BOOST  |
+|------------------------------|
+*/
 
-//PWM stuff
-
+/* current pseudo code
 read_voltage = digvolt(analogRead(1));
 read_current = analogRead(2)
 
@@ -93,15 +105,27 @@ if (power < prev_power) {
 }
 
 prev_power = power;
+*/
 
+/* DEMO 1 code*/
+
+int voltage = digvolt(analogRead(direcPin));
+// PWM value (0 - 4095)
+
+if (voltage <= 1) 
+   Tlc.set(0,1000);
+else if (voltage <=2 && voltage > 1)
+  Tlc.set(0, 2000);
+else if (voltage <=3 && voltage > 2)
+  Tlc.set(0, 3000);
+else
+  Tlc.set(0, 4000);   
+
+  
+Tlc.update();
+delay(500);   
 }
 
 float digvolt(float digital) {
     return digital*5/1023;
 }
-
-
-
-
-
-  
