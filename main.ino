a63e75dea285955c72c894c147880d0a25010dfa
allison98 <allison98@gmail.com>
2019-01-22 20:35:29 -0800

    stepper motor

--- main.ino
+++ main.ino
@@ -1,19 +1,36 @@
 /*
+Connect the Arduino Leonard and TLC4950 (SparkFun PWM Shield) the following way:
 
+Leo --- TLC4950
+
+5V -- 5V
+GND -- GND (one on each side) 
+ICSP inner center pin - SLCK (13)
+ICSP outer cennter pin - SIN (11)
+PIN 10 - BLANK (10)
+PIN 9 - XLAT (9)
+PIN 5 -- GSCLK (3)
+
+
+CONNECT THE 
 */
 
 #include <Stepper.h> // For the stepper motor
 
-#define STEPS 2038 // the number of steps in one revolution of your motor (28BYJ-48)
+#define STEPS 1023 // the number of steps in one revolution of your motor (28BYJ-48)
 
-Stepper stepper(STEPS, 8, 10, 9, 11);
+Stepper stepper(STEPS, 2, 4, 3, 6);
+
 int previous_direction = 0;
+float bc_current, bc_voltage, read_current, read_current;
+
+direcpin = 0 //
 
 void setup() {
     Serial.begin(9600); // connect to computer
 }
 
-void loop() {
+void loop() {   
 
 // Step 1: Measure all the variable
 
@@ -22,36 +39,27 @@ void loop() {
     // thats the value we want it to go
     // if the value is larger than 512 use 512 if the number is closer to 0 use 0
 
-val_direction = // measured value derivation
+
 
 // Step 3: Compare variables and change the respective actions
     // a) measure wind direction then control stepper motor
     // b) measure booster converter values and control PWM
+   //370 560 oohms 800
+
+val_direction = analogRead(direcPin);
 
 step_direction = previous_direction - val_direction; 
+
+/*
 if (step_direction > 512)
     step_direciton = 512;
 if (step direction < 0)
-    step_direction = 0; 
-
-// not the best idea
-/* some how create a way that relates step to resistance so we can directly do
-stepper.step(step_direction);
+    step_direction = 0;
 */
-switch (winddirection) {
-  case 1ohm:
-        stepper.step(step_direction);
-    break;
-  case 2ohm:
-        stepper.step(step_direction);    
-    break;
-  default:
-    // statements
-    break;
-}    
 
-previous_direction = val_direction
+stepper.step(step_direction);
 
+previous_direction = val_direction;
 
 
 
@@ -62,7 +70,34 @@ previous_direction = val_direction
     stepper.setSpeed(1); // 1 rpm
     stepper.step(-2038); // do 2038 steps in the other direction */
 
-    // Use for testing motor  
+    // Use for testing motor 
+
+
+//PWM stuff
+
+read_voltage = digvolt(analogRead(1));
+read_current = analogRead(2)
+
+bc_current = read_current;
+bc_voltage = read_voltage;
+
+power = bc_voltage * bc_current;
+
+
+if (power > prev_power) {
+    //decrease duty cycle
+}
+
+if (power < prev_power) {
+    //increase duty cycle
+}
+
+prev_power = power;
+
+}
+
+float digvolt(float digital) {
+    return digital*5/1023;
 }
 
 
